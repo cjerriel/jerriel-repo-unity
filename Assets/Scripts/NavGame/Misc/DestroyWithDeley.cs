464c47c7ac36e54eb2d4aa4ce1bd57f84a32ed3f
cjerriel <cjerriel13@gmail.com>
2020-06-03 11:43:56 -0300

    RedParticles prefab

---
+++ Assets/Scripts/NavGame/Misc/DestroyWithDeley.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyWithDeley : MonoBehaviour
+{
+    public float delay = 2f;
+    void Start()
+    {
+        Destroy(gameObject, delay);
+    }
+
+    void Update()
+    {
+        
+    }
+}
